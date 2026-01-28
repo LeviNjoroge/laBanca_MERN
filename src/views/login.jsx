ae616f445500e7d4aaccd899d18e5f15dce62750
Levi Njoroge <levynjoroge1@gmail.com>
2026-01-28 15:58:48 +0300

    working on a project: labanca, using React

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,8 +1,18 @@
+import React, { useState, useEffect } from 'react';
 
 function login(){
     return(
         <>
-        
+            <h2>Welcome back to LaBanca</h2>
+            <form action="" method="post">
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" placeholder="Joe Doe"/>
+                <br />
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password"/>
+                <br />
+                <button>Log in</button>
+            </form>
         </>
     );
 }
